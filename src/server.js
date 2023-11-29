32a93cadb8704306ce7f67adb3dbd9baed9e57a3
ameyabp <ameyap@umd.edu>
2023-11-28 16:34:02 -0800

    Added code to serve front end index.html via node express server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,27 @@
-import { port } from "./utility.js"
+import { port, frontendDir } from "./utility.js"
 import { Database } from "./database.js"
 
 import express from "express"
 
 function indexFunction(req, res) {
-    res.send("Hello World!");
+    // res.send("Hello World!");
+    const options = {
+        root: frontendDir
+    }
+    res.sendFile("index.html", options, (err) => {
+        if (err) {
+            console.log(err);
+            throw err;
+        }
+        else {
+            console.log("Sent index.html")
+        }
+    });
 } 
+
+function getData(req, res) {
+    console.log("Fetching data from DB");
+}
 
 function listener() {
     console.log(`Geospatial-graphs listening on port ${port}!`);
@@ -18,9 +34,11 @@ class Server {
     constructor(nodesFilePath, edgesFilePath) {
         this.db = new Database(nodesFilePath, edgesFilePath);
         this.app = express();
+        this.app.use(express.static(frontendDir));
 
         // register route methods for server
         this.app.get("/", indexFunction);
+        this.app.post("/", getData);
         this.app.listen(port, listener);
     }
 }
